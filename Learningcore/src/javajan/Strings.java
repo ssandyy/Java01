af422c5626bcc40ae2ef6f22c99d630c6695610b
sandeep <ssandyy789@gmail.com>
2023-02-28 12:57:43 -0500

    strings_concat_charat_contain_

--- Learningcore/src/javajan/Strings.java
+++ Learningcore/src/javajan/Strings.java
@@ -5,7 +5,25 @@ public class Strings {
         String name1 = "Sandy";
         String name2 = "bawa";
 
-        // Concatenation..
-        System.out.println(name1+ " "+name2);
+        // Concateination..
+        System.out.println(name1+ " "+name2);  //o/p:- Sandy bawa
+
+        name1 = "Mr."+ name1;
+        System.out.println(name1+ " "+name2);  // O/P: Mr. Sandy bawa
+
+        //CharAt(); to know character in specfic index
+        System.out.println(name1.charAt(3)); //O/P: S
+
+        //compareTo
+        System.out.println(name1.compareTo(name2));  //O/p: -21 (because its false and its indecate string alue gap)
+
+        //concat()
+        name2 = name2.concat(", How are you?");
+        System.out.println(name2);
+
+        //contains() -> to search element
+        System.out.println(name1.contains("sandy"));
+        System.out.println(name1.contains("bawa"));
+
     }
 }
