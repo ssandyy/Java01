a7351da79f8e6fd94edf2233b2c5972e9371828f
sandeep <ssandyy789@gmail.com>
2023-03-13 19:29:28 +0530

    Regex function on top of the program and uses mentioned

--- Learningcore/src/javajan/RegularExpression.java
+++ Learningcore/src/javajan/RegularExpression.java
@@ -2,6 +2,26 @@ package javajan;
 import java.util.regex.*;
 import java.util.regex.Pattern;
 
+/*
+ [abc]  -> a, b or c
+ [^abc] -> any character expect a,b,c
+ [a-z] -> a to z
+ [A-Z] -> A to Z
+ [a-zA-Z] -> a to z , A to Z
+ [0-9] -> 0 to 9
+
+ []?  ->  Occur 0 or 1 time only
+ []+  ->  Occurs 1 or more times.
+ []*  ->  Occur 0 or more times./**
+[]{n} -> Occurs n times
+[]{n,} -> Occurs n or more times
+[]{y,z} -> Occurs minimum y and max Z times.
+
+
+ */
+
+
+
 public class RegularExpression {
     public static void main(String[] args) {
         String sampleText = "Hello Coder, your email is coder789@gmail.com and phone number is 9955646562";
