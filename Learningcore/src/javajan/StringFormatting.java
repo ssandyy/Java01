1d3abc052a1a157523f72f274a01b052e1a747e8
sandeep <ssandyy789@gmail.com>
2023-03-13 17:48:16 +0530

    Stringformatting git commit -m Stringformatting

---
+++ Learningcore/src/javajan/StringFormatting.java
@@ -0,0 +1,23 @@
+package javajan;
+
+/*
+%s --> String
+%d --> digits
+*/
+
+public class StringFormatting {
+    public static void main(String[] args) {
+        
+        String greetingTemplete = "Hello, dear %s!, Good %s!";
+
+        String firstName = "sandy";
+        String morning = "morning";
+        String noon = "noon";
+        String night = "night";
+
+        String formattedString = String.format(greetingTemplete, firstName, morning);
+        System.out.println(formattedString);  // Hello, dear sandy!, Good morning!
+
+        System.out.printf("Now we are having %d subjects in class %d \n", 5,6);  // Now we are having 5 subjects in class 6
+    }
+}
